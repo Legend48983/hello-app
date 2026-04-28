603bdc47fbda6d218f0fc03c6e1748ac795a9d91
Aadhithya D <aadhithya48983@gmail.com>
2026-04-28 11:20:33 +0530

    UC5: Enhanced for loop with multiple arguments or default

---
+++ uc5.java
@@ -0,0 +1,13 @@
+public class Hello {
+    public static void main(String[] args) {
+
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            for (String name : args) {   // enhanced for loop
+                System.out.println("Hello " + name);
+            }
+        }
+
+    }
+}
